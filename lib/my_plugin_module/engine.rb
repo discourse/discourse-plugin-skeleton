fd24c37da46a5aee4844447f94a087b2db0916ff
David Taylor <david@taylorhq.com>
2024-05-02 18:45:43 +0100

    DEV: Add eager loading for scheduled jobs

--- lib/my_plugin_module/engine.rb
+++ lib/my_plugin_module/engine.rb
@@ -5,5 +5,9 @@ module ::MyPluginModule
     engine_name PLUGIN_NAME
     isolate_namespace MyPluginModule
     config.autoload_paths << File.join(config.root, "lib")
+    scheduled_job_dir = "#{config.root}/app/jobs/scheduled"
+    config.to_prepare do
+      Rails.autoloaders.main.eager_load_dir(scheduled_job_dir) if Dir.exist?(scheduled_job_dir)
+    end
   end
 end
